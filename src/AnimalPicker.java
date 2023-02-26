6eba3e81ed6fbd9b3a79d8258aa56c7c8dfe4486
hawkeyephil <hawkeyephil@icloud.com>
2023-02-25 23:53:38 -0500

    Require correct input

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
-import org.jcp.xml.dsig.internal.dom.DOMPGPData;
-
 //Program that displays ASCII art of an animal of the users choosing 
 public class AnimalPicker {
     public static void main(String[] args) {
         //Ask user for animal to draw
         System.out.println("Hello, please enter 'cat' or 'dog' for Art!");
         Scanner scanner = new Scanner(System.in);
-        String animal = scanner.nextLine();
+        String animal = ""; //scanner.nextLine();
 
-        //Return an error if cat or dog is not entered
-        if(!animal.equals("cat") && !animal.equals("dog")) {
-            System.out.println("Invalid Input.");
+        //Ask user for input until cat or dog is returned
+        while(!animal.equals("cat") && !animal.equals("dog")) {
+            animal = scanner.nextLine(); 
+            //Return the animal art requested or ask user again
+            if(animal.equals("cat")) {
+                System.out.println("Cat ASCII Art Placeholder");
+            }
+            else if(animal.equals("dog")) {
+                System.out.println("Dog ASCII Art Placeholder");
+            }
+            else {
+                System.out.println("Invalid Input, Try Again!");
+            }
         }
 
-        //Return the animal art requested
-        if(animal.equals("cat")) {
-            System.out.println("Cat ASCII Art Placeholder");
-        }
-        if(animal.equals("dog")) {
-            System.out.println("Dog ASCII Art Placeholder");
-        }
         scanner.close();
     }
 }
