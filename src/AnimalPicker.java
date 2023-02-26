096364d72c7c7b492830486e47cf531983667736
hawkeyephil <hawkeyephil@icloud.com>
2023-02-25 23:17:54 -0500

    User Selection

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+import org.jcp.xml.dsig.internal.dom.DOMPGPData;
+
+//Program that displays ASCII art of an animal of the users choosing 
 public class AnimalPicker {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Ask user for animal to draw
+        System.out.println("Hello, please enter 'cat' or 'dog' for Art!");
+        Scanner scanner = new Scanner(System.in);
+        String animal = scanner.nextLine();
+
+        //Return the animal art requested
+        if(animal.equals("cat")) {
+            System.out.println("Cat ASCII Art Placeholder");
+        }
+        else if(animal.equals("dog")) {
+            System.out.println("Dog ASCII Art Placeholder");
+        }
+        else {
+            System.out.println("Invalid Input.");
+        }
+        scanner.close();
     }
 }
